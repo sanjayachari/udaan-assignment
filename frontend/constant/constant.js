31eca83476dce36036f34dcf7b9487021ebd1cb8
sanjay <sanjayacharya992@gmail.com>
2024-12-30 22:45:17 +0530

    update of credentials in axios post

--- frontend/constant/constant.js
+++ frontend/constant/constant.js
@@ -1,5 +1,5 @@
-const BACKEND = 'http://localhost:5000/server/v1'
-// const BACKEND = 'https://udaan-backend-assignment.onrender.com/server/v1'
+// const BACKEND = 'http://localhost:5000/server/v1'
+const BACKEND = 'https://udaan-backend-assignment.onrender.com/server/v1'
 
 
 module.exports = { BACKEND }
